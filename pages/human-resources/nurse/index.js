5fdb27766d94b744594f2ebe7fc1e4e8c4a7e29c
Neal Dam <neal.dam.1008@gmail.com>
2021-01-02 19:44:30 +0700

    test auto deploy

--- pages/human-resources/nurse/index.js
+++ pages/human-resources/nurse/index.js
@@ -3,24 +3,26 @@ import useSWR from "swr";
 import NurseTable from "../../../src/components/NurseTable";
 import { GET_ALL_NURSES_URL } from "../../../src/constants/url";
 import fetcher from "../../../src/fetcher";
-import { defaultPage } from "../../../src/hocs/defaultPage"
+import { defaultPage } from "../../../src/hocs/defaultPage";
 import { protectRoute } from "../../../src/hocs/protectRoute";
 
 function HumanResourcesNursePage() {
-
   const route = useRouter();
   const { data, error } = useSWR(GET_ALL_NURSES_URL, fetcher);
 
   const handleClickNurseProfile = (nurseId) => {
     route.push(route.asPath + "/" + nurseId + "/profile");
-  }
+  };
 
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
 
   return (
-    <NurseTable handleClickNurseProfile={handleClickNurseProfile} nurses={data} />
-  )
+    <NurseTable
+      handleClickNurseProfile={handleClickNurseProfile}
+      nurses={data}
+    />
+  );
 }
 
 export default protectRoute(defaultPage(HumanResourcesNursePage));
